013c2f5f9c15152a940da2b08e20af8abea3aec3
Fernandes <ferns3005@gmail.com>
2025-12-16 22:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const names = [
+  "James","Margaret","Muriel","Fergal",
+  "Jess","Hilda","Giles","Shannon","Anna",
+  "Treza","Alister","Contrad"
+];
+
+// show moving names
+const wheel = document.getElementById("wheel");
+const result = document.getElementById("result");
+
+let i = 0;
+
+// spin animation
+const spin = setInterval(() => {
+  wheel.innerText = names[i % names.length];
+  i++;
+}, 120);
+
+// stop on JESS
+setTimeout(() => {
+  clearInterval(spin);
+  wheel.innerText = "Jess";
+  result.innerText = "🎁 You got: JESS";
+}, 3000);
